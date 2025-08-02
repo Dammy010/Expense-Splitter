f4b07d629fde65c9ad76e275d0a38396f7c2b91a
Damilare <da9783790@gmil.com>
2025-08-02 18:49:37 +0100

    Expense Splitter commit

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,12 +1,18 @@
 import axios from 'axios';
 
+const isLocalhost = window.location.hostname === 'localhost';
+
 const API = axios.create({
-  baseURL: 'https://expense-splitter-9ox1.vercel.app',
+  baseURL: isLocalhost
+    ? 'http://localhost:5000' 
+    : 'https://expense-splitter-9ox1.vercel.app', 
 });
 
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem('token');
-  if (token) req.headers.Authorization = `Bearer ${token}`;
+  if (token) {
+    req.headers.Authorization = `Bearer ${token}`;
+  }
   return req;
 });
 
